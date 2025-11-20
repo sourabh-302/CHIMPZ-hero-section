adf9ed48218286257b132bc7bcf2fe2a0f8ad60a
sourabh-302 <sourabhgadhwal499@gmail.com>
2025-11-20 10:13:16 -0700

    recheck

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,13 @@ import About from "./components/pages/About.jsx";
 import Buy from "./components/pages/Buy.jsx";
 import Tokenomics from "./components/pages/Tokenomics.jsx";
 import Roadmap from "./components/pages/Roadmap.jsx";
-import Discord from "./components/pages/Discord.jsx";
-import Twitter from "./components/pages/Twitter.jsx";
-import Telegram from "./components/pages/Telegram.jsx";
+ 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <section className="bg-[url('/src/assets/bg.png')] min-h-screen bg-cover bg-center bg-no-repeat">
+      <section className="bg-[url('/src/assets/bg.png')] min-h-screen bg-cover bg-center bg-no-repeat flex flex-col flex-nowrap">
         
         <Header />
 
@@ -24,9 +22,7 @@ function App() {
           <Route path="/buy" element={<Buy />} />
           <Route path="/tokenomics" element={<Tokenomics />} />
           <Route path="/roadmap" element={<Roadmap />} />
-          <Route path="/discord" element={<Discord />} />
-          <Route path="/twitter" element={<Twitter />} />
-          <Route path="/telegram" element={<Telegram />} />
+          
         </Routes>
 
       </section>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,14 @@
+import image from "../../assets/gorilla.png";
+
 export default function Home() {
   return (
+    <div className="flex grow justify-center items-center "> 
     <div className="max-w-[1440px] mx-auto px-3">
-      <div className="flex flex-row flex-wrap -mx-3 grow items-center">
+      <div className="flex flex-row flex-wrap -mx-3 items-center">
+        
         <div className="flex w-full md:w-[58.33%] px-3">
           <div className="flex flex-col justify-center bg-[linear-gradient(180deg,rgba(255,255,255,0)_0%,rgba(255,255,255,0.01)_85.54%,rgba(255,255,255,0)_100%)] backdrop-blur-[3px] pt-8 pb-8 px-2">
+            
             <h1 className="font-roboto font-semibold text-[16px] text-white [text-shadow:1px_1px_4px_rgb(255,255,255)]">
               APE TOGETHER STRONG
             </h1>
@@ -21,17 +26,21 @@ export default function Home() {
               a safe haven for all the risk-takers and degenerates who have
               their helmets ready for the next moon landing.
             </p>
+
           </div>
         </div>
 
         <div className="w-full md:w-[41.66%] px-3 flex justify-center">
           <img
-            src="/src/assets/gorilla.png"
+            src={image}
             alt="gorilla"
             className="flex justify-center gorilla"
           />
         </div>
+
       </div>
+    </div>
     </div>
   );
 }
+
